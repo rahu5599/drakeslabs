47f28549d54a3020e9aa61227de99b4838a96f50
rahul5599 <rahul.mashere@xecurify.com>
2021-12-08 12:24:15 +0530

    done

--- custom.js
+++ custom.js
@@ -17,8 +17,8 @@ $(function() {
     safari = /safari/.test( userAgent ),
     ios = /iphone|ipod|ipad/.test( userAgent );
 
-    // if( ios && !standalone && !safari) {
-        if(true){
+    if( ios && !standalone && !safari) {
+      
                         var stylesheet = `
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
